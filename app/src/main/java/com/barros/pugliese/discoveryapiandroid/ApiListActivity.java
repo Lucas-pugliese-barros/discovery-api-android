4b5187ac66c751e19ae87a0d0e3e7cbdaad60aa5
Lucas Pugliese De Morais Barros <lucas.pugliese.barros@gmail.com>
2019-01-17 20:13:07 -0300

    Initial struct

---
+++ app/src/main/java/com/barros/pugliese/discoveryapiandroid/ApiListActivity.java
@@ -0,0 +1,13 @@
+package com.barros.pugliese.discoveryapiandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApiListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_list);
+    }
+}
